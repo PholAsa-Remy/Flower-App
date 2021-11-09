9d58e1cb8dd5b284451d7342af000df82fe161ba
CHHOA thierry <thierry.chhoa@etu.univ-paris-diderot.fr>
2021-11-09 22:21:35 +0100

    add binding in recherche, main and modify

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate( layoutInflater )
+        setContentView( binding.root)
 
-        //test
-        //test 2
-        //test 3
     }
 }
--- app/src/main/java/com/example/project/RechercheActivity.kt
+++ app/src/main/java/com/example/project/RechercheActivity.kt
@@ -2,10 +2,16 @@ package com.example.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project.databinding.ActivityMainBinding
+import com.example.project.databinding.ActivityRechercheBinding
 
 class RechercheActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityRechercheBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recherche)
+        binding = ActivityRechercheBinding.inflate( layoutInflater )
+        setContentView( binding.root)
     }
 }
