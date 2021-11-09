c809207bde5de810eaa88173a2603168ab93a789
Remy Phol Asa <pholasa.remy@outlook.fr>
2021-11-09 22:43:40 +0100

    Add binding to recall and add

--- app/src/main/java/com/example/project/AddActivity.kt
+++ app/src/main/java/com/example/project/AddActivity.kt
@@ -2,10 +2,16 @@ package com.example.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project.databinding.ActivityAddBinding
+import com.example.project.databinding.ActivityMainBinding
 
 class AddActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityAddBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add)
+        binding = ActivityAddBinding.inflate( layoutInflater )
+        setContentView( binding.root)
     }
 }
