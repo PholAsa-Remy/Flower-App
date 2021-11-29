2e186141f037a014f03b8739501077ab7a8230c8
Remy Phol Asa <pholasa.remy@outlook.fr>
2021-11-29 12:36:11 +0100

    Saving and loading photo

--- app/src/main/java/com/example/project/AddActivity.kt
+++ app/src/main/java/com/example/project/AddActivity.kt
@@ -1,30 +1,45 @@
 package com.example.project
 
 import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.icu.text.SimpleDateFormat
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModelProvider
 import com.example.project.databinding.ActivityAddBinding
 import com.example.project.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.*
+import java.util.*
+
 
 class AddActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityAddBinding
     lateinit var flower : Flower
     lateinit var model : FlowerViewModel
+    lateinit var imageBitmap : Bitmap
 
     val launcher: ActivityResultLauncher<Intent> = registerForActivityResult (
         ActivityResultContracts.StartActivityForResult())
     { //listener
         if (it.resultCode == Activity.RESULT_OK){
-            val imageBitmap = it.data?.extras?.get("data") as Bitmap
+            imageBitmap = it.data?.extras?.get("data") as Bitmap
             binding.flowerPicture.setImageBitmap(imageBitmap)
+            savePhoto ("a", imageBitmap)
+            binding.imageView.setImageBitmap(loadPhoto("a.jpg"))
         }
     }
 
@@ -46,16 +61,14 @@ class AddActivity : AppCompatActivity() {
         binding.flowerPicture.setOnClickListener(){
             val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             launcher.launch(takePictureIntent)
-
         }
+
         binding.bAddFlower.setOnClickListener(){
             val name = binding.edName.text.toString()
             val picture = binding.edPicture.text.toString()
             val period = binding.edPeriod.text.toString()
             val nextWatering = binding.edNextWatering.text.toString()
             val frequency = binding.edFrequency.text.toString()
-
-
 
             if (name == "" || picture == "" || period == "" || nextWatering == "" || frequency == "" || frequency.toInt() <= 0){
                 Toast.makeText(this, "Some field are missing", Toast.LENGTH_SHORT).show()
@@ -65,4 +78,25 @@ class AddActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun loadPhoto (filename : String) : Bitmap {
+        var file = getFilesDir()
+        var f = File(file, filename)
+        var b : Bitmap = BitmapFactory.decodeStream(FileInputStream(f))
+        return b
+    }
+    private fun savePhoto (filename : String, bmp : Bitmap) : Boolean {
+        return try {
+            // Need Output stream
+            openFileOutput("$filename.jpg", MODE_PRIVATE).use { stream ->
+                if(!bmp.compress(Bitmap.CompressFormat.JPEG, 95, stream)){
+                    throw IOException ("Couldn't save bitmap.")
+                }
+            }
+            true
+        }catch (e : IOException){
+            false
+        }
+    }
+
 }
