fece27f4e606baf1cf9d26c465d078536c85462c
Remy Phol Asa <pholasa.remy@outlook.fr>
2021-12-06 08:43:09 +0100

    Add photo to modify

--- app/src/main/java/com/example/project/ModifyActivity.kt
+++ app/src/main/java/com/example/project/ModifyActivity.kt
@@ -1,17 +1,34 @@
 package com.example.project
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.ViewModelProvider
 import com.example.project.databinding.ActivityModifyBinding
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ModifyActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityModifyBinding
     lateinit var model :FlowerViewModel
+    lateinit var flower : Flower
+    lateinit var imageBitmap : Bitmap
 
+    val launcher: ActivityResultLauncher<Intent> = registerForActivityResult (
+        ActivityResultContracts.StartActivityForResult())
+    { //listener
+        if (it.resultCode == Activity.RESULT_OK){
+            imageBitmap = it.data?.extras?.get("data") as Bitmap
+            binding.flowerPicture.setImageBitmap(imageBitmap)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +43,18 @@ class ModifyActivity : AppCompatActivity() {
         //Fill the information
         model.flowers.observe(this) {
             if (it.size == 1){
-                binding.edName.setText(it[0].name)
-                binding.edLatinName.setText(it[0].latinName)
-                binding.edFrequency.setText(it[0].frequency)
-                binding.edNutrimentFrequency.setText(it[0].nutrimentFrequency.toString())
+                flower = model.flowers.value?.get(0)!!
+                binding.edName.setText(flower.name)
+                binding.edLatinName.setText(flower.latinName)
+                binding.edFrequency.setText(flower.frequency)
+                binding.edNutrimentFrequency.setText(flower.nutrimentFrequency.toString())
+                if (flower.picture != "None"){
+                    binding.flowerPicture.setImageBitmap(PhotoManager.loadPhoto(flower.picture, this))
+                }
             }
+        }
+        binding.flowerPicture.setOnClickListener(){
+            PhotoManager.takePhoto(launcher)
         }
 
         binding.bModifyFlower.setOnClickListener(){
@@ -42,11 +66,18 @@ class ModifyActivity : AppCompatActivity() {
             if (name == "" || latinName == "" || frequency == "" || nutrimentFrequency == "" || nutrimentFrequency.toInt() <= 0){
                 Toast.makeText(this, "Some field are missing", Toast.LENGTH_SHORT).show()
             }else{
-                val flower = model.flowers.value?.get(0)!!
+                //val flower = model.flowers.value?.get(0)!!
                 flower.name = name
                 flower.latinName = latinName
                 flower.frequency = frequency
                 flower.nutrimentFrequency = nutrimentFrequency.toInt()
+                if (this::imageBitmap.isInitialized) {
+                    val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+                    PhotoManager.savePhoto (timeStamp, imageBitmap,this)
+                    flower.picture = "$timeStamp.jpg"
+                }else{
+                    flower.picture = "None"
+                }
                 model.updateFlower (flower)
 
                 var goToResearch : Intent = Intent (this, ResearchActivity:: class.java)
@@ -56,7 +87,8 @@ class ModifyActivity : AppCompatActivity() {
         }
 
         binding.bDeleteFlower.setOnClickListener(){
-            model.deleteFlower(model.flowers.value?.get(0)!!)
+            //model.deleteFlower(model.flowers.value?.get(0)!!)
+            model.deleteFlower(flower)
             var goToResearch : Intent = Intent (this, ResearchActivity:: class.java)
             setResult(RESULT_OK,goToResearch)
             finish()
